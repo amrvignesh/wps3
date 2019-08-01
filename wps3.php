a3d8b5631385ac6add4218bbb160cd8518c578ff
Vignesh <vraja@gsu.edu>
2019-08-01 08:23:38 -0400

    Setup S3 Lin - Using standard AWS S3library

--- wps3.php
+++ wps3.php
@@ -5,3 +5,19 @@ Description: Offload WordPress uploads directory to S3 compatible storage
 Version: 0.1
 Author: Vignesh AMR
 */
+
+// Define AWS SDK dependency
+require_once(ABSPATH . 'wp-content/plugins/s3-uploads/vendor/autoload.php');
+
+use Aws\S3\S3Client;
+use Aws\S3\Exception\S3Exception;
+
+// Initialize the S3 client
+$s3_client = new S3Client([
+    'version' => 'latest',
+    'region' => 'your_s3_bucket_region',
+    'credentials' => [
+        'key' => 'your_s3_access_key',
+        'secret' => 'your_s3_secret_key',
+    ],
+]);
