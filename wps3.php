157716005ad8e7c5f6b28e51fb34146090f402cc
Vignesh <vraja@gsu.edu>
2019-09-19 01:48:42 -0400

    Upload files to specified bucket.

--- wps3.php
+++ wps3.php
@@ -21,3 +21,46 @@ $s3_client = new S3Client([
         'secret' => 'your_s3_secret_key',
     ],
 ]);
+
+// Hook into WordPress actions
+add_filter('upload_dir', 's3_uploads_offloader');
+
+// Function to offload uploads folder to S3
+function s3_uploads_offloader($uploads) {
+    global $s3_client;
+
+    // Set S3 bucket name
+    $bucket_name = 'your_s3_bucket_name';
+
+    // Check if the bucket exists, create if necessary
+    try {
+        $s3_client->headBucket(['Bucket' => $bucket_name]);
+    } catch (S3Exception $e) {
+        $s3_client->createBucket(['Bucket' => $bucket_name]);
+    }
+
+    // Move existing files to S3 bucket
+    $local_uploads_dir = wp_upload_dir();
+    $local_files = scandir($local_uploads_dir['path']);
+    foreach ($local_files as $file) {
+        if ($file === '.' || $file === '..') {
+            continue;
+        }
+        $local_file_path = $local_uploads_dir['path'] . '/' . $file;
+        $s3_key = 'your_desired_folder_in_s3_bucket/' . $file;
+        $s3_client->putObject([
+            'Bucket' => $bucket_name,
+            'Key' => $s3_key,
+            'SourceFile' => $local_file_path,
+        ]);
+        // Delete the local file after successful upload to S3 (optional)
+        unlink($local_file_path);
+    }
+
+    // Configure WordPress to use S3 bucket for future uploads
+    $uploads['baseurl'] = "https://{$bucket_name}.s3.amazonaws.com";
+    $uploads['url'] = $uploads['baseurl'] . '/your_desired_folder_in_s3_bucket';
+    $uploads['path'] = wp_normalize_path(WP_CONTENT_DIR . '/uploads');
+
+    return $uploads;
+}
