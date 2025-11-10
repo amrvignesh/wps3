c27e84b2854ab00d3e5c331473f45121bd88a5a1
Vignesh R. <amrv1990@gmail.com>
2025-11-10 11:27:15 -0500

    Add option for periodic automatic migrations

--- wps3.php
+++ wps3.php
@@ -514,3 +514,4 @@ function filter_by_date($files, $date_from, $date_to) { }
 <input type=\"number\" name=\"endpoint_port\"> 
 <input type=\"text\" name=\"path_prefix\"> 
 <input type=\"number\" name=\"request_timeout\" min=\"5\" max=\"120\"> 
+<select name=\"periodic_migration\"> 
