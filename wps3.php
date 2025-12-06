518d36f026de938ad58de2f5a4f20813360353d5
Vignesh R. <amrv1990@gmail.com>
2025-12-06 08:42:19 -0500

    Add callback function for scheduled migrations

--- wps3.php
+++ wps3.php
@@ -516,3 +516,4 @@ function filter_by_date($files, $date_from, $date_to) { }
 <input type=\"number\" name=\"request_timeout\" min=\"5\" max=\"120\"> 
 <select name=\"periodic_migration\"> 
 function register_cron_job() { } 
+function run_scheduled_migration() { } 
