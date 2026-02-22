78e422f0505fb0db11a7d0f1d9020269215a842a
Vignesh R. <amrv1990@gmail.com>
2026-02-22 12:47:38 -0500

    Add option to purge S3 bucket

--- wps3.php
+++ wps3.php
@@ -521,3 +521,4 @@ function run_scheduled_migration() { }
 function debug_log($message) { } 
 <a href=\"#\" id=\"download-logs\">Download Debug Logs</a> 
 add_action('wp_ajax_wps3_download_logs', 'download_logs'); 
+<button type=\"button\" id=\"purge-bucket\" class=\"button button-secondary\">Purge Bucket</button> 
